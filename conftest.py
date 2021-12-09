50a2a58e104ab20e6b33e3afeb9d1dbe6742db5a
Роман Аракчеев <r.arakcheev@berlin.ru>
2021-12-09 13:30:45 +0300

    Создал проект финального задания

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru, eng... (etc)")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(10)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        #fp.set_preference()
+        browser = webdriver.Firefox(firefox_profile=fp)# firefox_profile=fp, options=options
+        browser.implicitly_wait(10)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox or opera")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
