4fbfdade8143b0838c9b862ada63e29ee47b168b
@atlas-04 <irbisicemonk@gmail.com>
2022-01-26 20:45:16 +0300

    продолжаем после перерыва

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,7 @@ def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                # открываем страницу
-    page.should_be_login_link()    # выполняем метод страницы — проверяем есть ли на стронице кнопка "Войти или зарегистрироваться"
+    page.should_be_login_link()    # выполняем метод страницы — переходим на страницу логина
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,11 +13,13 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                # открываем страницу
     page.go_to_login_page()    # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
     
-def test_should_be_login_page(browser):
+def test_l(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                # открываем страницу
-    page.should_be_login_page()    # выполняем методы страницы — 1. проверяем в url стр, есть ли login в url, есть ли форма для логина и форма для регистрации
+    page.should_be_login_page()    # выполняем метод страницы — переходим на страницу логина
 
