ea7243937892168cacba7a4064983b0af081adf8
Your Name <you@example.com>
2022-02-17 16:40:46 +0300

    добавил начальный тест по добавлению товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -3,19 +3,23 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    
+
+
 class LoginPageLocators():
     REG_FORM = (By.ID, "register_form")
     REG_EMAIL = (By.ID, "id_registration-email")
     REG_PASSWORD = (By.ID, "id_registration-password1")
     REG_REPEAT_PASSWORD = (By.ID, "id_registration-password2")
     REG_BUTTON = (By.CSS_SELECTOR, "#register_form > button")
-    
+
     LOGIN_FORM = (By.ID, "login_form")
     LOGIN_EMAIL = (By.ID, "id_login-username")
     LOGIN_PASSWORD = (By.ID, "id_login-password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "#login_form > button")
     FORGOT_PASSWORD_BUTTON = (By.CSS_SELECTOR, "#login_form > p > a")
-    
+
+
 class CatalogPageLocators():
     BASKET_BUTTON = (By.XPATH, '//*[@id="add_to_basket_form"]/button')
+    ITEM_IN_BASKET = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
+    ITEM_ON_PAGE = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > h1')
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,23 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
-from .locators import LoginPageLocators
+from .locators import CatalogPageLocators
 
 class ProductPage(BasePage):
 
-    def add_to_basket(self)
+    def should_be_basket_button(self):
+        assert self.is_element_present(*CatalogPageLocators.BASKET_BUTTON), "Add to basket is not presented"
+
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*CatalogPageLocators.BASKET_BUTTON)
+        add_to_basket_button.click()
+
+    def should_be_item_in_basket(self):
+        assert self.is_element_present(*CatalogPageLocators.ITEM_IN_BASKET), "Item in basket message is not presented"
+
+    def name_equal(self):
+        item_basket = self.browser.find_element(*CatalogPageLocators.ITEM_IN_BASKET)
+        item_name_in_basket = item_basket.get_attribute('innerHTML')
+        print(item_name_in_basket)
+        item_page = self.browser.find_element(*CatalogPageLocators.ITEM_ON_PAGE)
+        item_name_on_page = item_page.get_attribute('innerHTML')
+        print(item_name_on_page)
+        assert item_name_in_basket == item_name_on_page
